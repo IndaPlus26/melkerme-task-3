68a2f74cda7fc3088d5ca36a74c57122c6af6882
melnder <3melker@gmail.com>
2026-09-15 13:22:41 +0200

    general bugfixes and updated README

--- src/board.rs
+++ src/board.rs
@@ -41,7 +41,13 @@ impl Board {
 
         // Get the white turn
         board.current_color_turn = match fen_iter.next() {
-            Some(white_move) => if white_move == "w" { piece::WHITE } else { piece::BLACK },
+            Some(white_move) => {
+                if white_move == "w" {
+                    piece::WHITE
+                } else {
+                    piece::BLACK
+                }
+            }
             None => panic!("Invalid FEN string"),
         };
 
@@ -59,20 +65,18 @@ impl Board {
                     }
                 }
                 rights
-            },
+            }
             None => panic!("Invalid FEN string"),
         };
 
         // Get the en passant square
         board.en_passant_square = match fen_iter.next() {
-            Some(en_passant_square) => {
-                match en_passant_square {
-                    "-" => 64,
-                    _ => {
-                        let file = en_passant_square.chars().nth(0).unwrap() as usize - b'a' as usize;
-                        let rank = en_passant_square.chars().nth(1).unwrap() as usize - b'1' as usize;
-                        file + rank * 8
-                    }
+            Some(en_passant_square) => match en_passant_square {
+                "-" => 64,
+                _ => {
+                    let file = en_passant_square.chars().nth(0).unwrap() as usize - b'a' as usize;
+                    let rank = en_passant_square.chars().nth(1).unwrap() as usize - b'1' as usize;
+                    file + (7 - rank) * 8
                 }
             },
             None => panic!("Invalid FEN string"),
@@ -89,7 +93,7 @@ impl Board {
             Some(fullmove) => fullmove.parse().unwrap(),
             None => 1,
         };
-        
+
         // Parse and set the board state
         let mut i = 0;
         for c in board_state {
@@ -143,7 +147,11 @@ impl Board {
         self.en_passant_square
     }
     pub fn swap_current_turn_color(&mut self) {
-        self.current_color_turn = if self.current_color_turn == piece::WHITE { piece::BLACK } else { piece::WHITE };
+        self.current_color_turn = if self.current_color_turn == piece::WHITE {
+            piece::BLACK
+        } else {
+            piece::WHITE
+        };
     }
 
     pub fn is_in_check(&self, color: Option<u8>) -> bool {
@@ -152,8 +160,12 @@ impl Board {
             Some(color) => color,
             None => self.current_color_turn,
         };
-        let attacking_color = if defending_color == piece::WHITE { piece::BLACK } else { piece::WHITE };
-        
+        let attacking_color = if defending_color == piece::WHITE {
+            piece::BLACK
+        } else {
+            piece::WHITE
+        };
+
         for i in 0..64 {
             // Get king pos and check if attacked
             if self.get_square(i) == piece::create(piece::KING, defending_color) {
@@ -173,8 +185,13 @@ impl Board {
         };
         for offset in pawn_offsets {
             let new_square = square as isize + offset;
-            if new_square >= 0 && new_square < 64 && (new_square as usize % 8).abs_diff(square % 8) == 1 {
-                if self.get_square(new_square as usize) == piece::create(piece::PAWN, attacking_color) {
+            if new_square >= 0
+                && new_square < 64
+                && (new_square as usize % 8).abs_diff(square % 8) == 1
+            {
+                if self.get_square(new_square as usize)
+                    == piece::create(piece::PAWN, attacking_color)
+                {
                     return true;
                 }
             }
@@ -184,8 +201,14 @@ impl Board {
         let knight_offsets = [-17, -15, -10, -6, 6, 10, 15, 17];
         for offset in knight_offsets {
             let new_square = square as isize + offset;
-            if new_square >= 0 && new_square < 64 && (new_square as usize % 8).abs_diff(square % 8) <= 2 && (new_square as usize / 8).abs_diff(square / 8) <= 2 {
-                if self.get_square(new_square as usize) == piece::create(piece::KNIGHT, attacking_color) {
+            if new_square >= 0
+                && new_square < 64
+                && (new_square as usize % 8).abs_diff(square % 8) <= 2
+                && (new_square as usize / 8).abs_diff(square / 8) <= 2
+            {
+                if self.get_square(new_square as usize)
+                    == piece::create(piece::KNIGHT, attacking_color)
+                {
                     return true;
                 }
             }
@@ -194,25 +217,25 @@ impl Board {
         // Sliding piece attacks
         let direction_offsets: [isize; 8] = [
             -1, // Left
-            1, // Right
+            1,  // Right
             -8, // Up
-            8, // Down
+            8,  // Down
             -9, // Up-left
             -7, // Up-right
-            7, // Down-left
-            9, // Down-right
+            7,  // Down-left
+            9,  // Down-right
         ];
         let to_edges = [
-            square % 8, // Left edge
+            square % 8,       // Left edge
             7 - (square % 8), // Right edge
-            square / 8, // Up edge
+            square / 8,       // Up edge
             7 - (square / 8), // Down edge
         ];
         let length_to_edges = [
-            to_edges[0], // Left edge
-            to_edges[1], // Right edge
-            to_edges[2], // Up edge
-            to_edges[3], // Down edge
+            to_edges[0],                  // Left edge
+            to_edges[1],                  // Right edge
+            to_edges[2],                  // Up edge
+            to_edges[3],                  // Down edge
             to_edges[0].min(to_edges[2]), // Up-left edge
             to_edges[1].min(to_edges[2]), // Up-right edge
             to_edges[0].min(to_edges[3]), // Down-left edge
@@ -220,7 +243,6 @@ impl Board {
         ];
         for dir_index in 0..8 {
             for n in 0..length_to_edges[dir_index] {
-
                 // This feels like magic but makes sense if you think about it
                 let new_square = square as isize + direction_offsets[dir_index] * (n + 1) as isize;
 
@@ -235,19 +257,26 @@ impl Board {
                     }
 
                     let attacking_piece = piece::get_piece(attacking_piece);
-                    if attacking_piece == piece::QUEEN { return true }
-                    if attacking_piece == piece::ROOK && dir_index < 4 { return true } // Striaghts are the first 4 directions
-                    if attacking_piece == piece::BISHOP && dir_index >= 4 { return true } // Diagonals are the last 4 directions
-                    if attacking_piece == piece::KING && n == 0 { return true } // King controls the closest square in every direction
+                    if attacking_piece == piece::QUEEN {
+                        return true;
+                    }
+                    if attacking_piece == piece::ROOK && dir_index < 4 {
+                        return true;
+                    } // Striaghts are the first 4 directions
+                    if attacking_piece == piece::BISHOP && dir_index >= 4 {
+                        return true;
+                    } // Diagonals are the last 4 directions
+                    if attacking_piece == piece::KING && n == 0 {
+                        return true;
+                    } // King controls the closest square in every direction
                     break;
                 }
             }
         }
 
-
         false
     }
-    fn get_pseudo_legal_moves(&self) -> Option<Vec<Move>> { 
+    fn get_pseudo_legal_moves(&self) -> Option<Vec<Move>> {
         let mut moves: Vec<Move> = Vec::with_capacity(64);
         for i in 0..64 {
             if piece::get_color(self.get_square(i)) != self.current_color_turn {
@@ -286,7 +315,7 @@ impl Board {
         }
         Some(moves)
     }
-    pub fn get_legal_moves(&self) -> Option<Vec<Move>> { 
+    pub fn get_legal_moves(&self) -> Option<Vec<Move>> {
         let pseudo_legal_moves = self.get_pseudo_legal_moves();
         if pseudo_legal_moves.is_none() {
             return None;
@@ -303,10 +332,14 @@ impl Board {
                     if self.get_square(63) != piece::create(piece::ROOK, piece::WHITE) {
                         continue;
                     }
-                    if !piece::is_empty(self.get_square(61)) || !piece::is_empty(self.get_square(62)) {
+                    if !piece::is_empty(self.get_square(61))
+                        || !piece::is_empty(self.get_square(62))
+                    {
                         continue;
                     }
-                    if self.is_square_controlled(61, piece::BLACK) || self.is_square_controlled(60, piece::BLACK) {
+                    if self.is_square_controlled(61, piece::BLACK)
+                        || self.is_square_controlled(60, piece::BLACK)
+                    {
                         continue;
                     }
                 } else if m.to == 58 {
@@ -316,10 +349,15 @@ impl Board {
                     if self.get_square(56) != piece::create(piece::ROOK, piece::WHITE) {
                         continue;
                     }
-                    if !piece::is_empty(self.get_square(59)) || !piece::is_empty(self.get_square(58)) || !piece::is_empty(self.get_square(57)) {
+                    if !piece::is_empty(self.get_square(59))
+                        || !piece::is_empty(self.get_square(58))
+                        || !piece::is_empty(self.get_square(57))
+                    {
                         continue;
                     }
-                    if self.is_square_controlled(59, piece::BLACK) || self.is_square_controlled(60, piece::BLACK) {
+                    if self.is_square_controlled(59, piece::BLACK)
+                        || self.is_square_controlled(60, piece::BLACK)
+                    {
                         continue;
                     }
                 } else if m.to == 6 {
@@ -329,10 +367,13 @@ impl Board {
                     if self.get_square(7) != piece::create(piece::ROOK, piece::BLACK) {
                         continue;
                     }
-                    if !piece::is_empty(self.get_square(5)) || !piece::is_empty(self.get_square(6)) {
+                    if !piece::is_empty(self.get_square(5)) || !piece::is_empty(self.get_square(6))
+                    {
                         continue;
                     }
-                    if self.is_square_controlled(5, piece::WHITE) || self.is_square_controlled(4, piece::WHITE) {
+                    if self.is_square_controlled(5, piece::WHITE)
+                        || self.is_square_controlled(4, piece::WHITE)
+                    {
                         continue;
                     }
                 } else if m.to == 2 {
@@ -342,10 +383,15 @@ impl Board {
                     if self.get_square(0) != piece::create(piece::ROOK, piece::BLACK) {
                         continue;
                     }
-                    if !piece::is_empty(self.get_square(3)) || !piece::is_empty(self.get_square(2)) || !piece::is_empty(self.get_square(1)) {
+                    if !piece::is_empty(self.get_square(3))
+                        || !piece::is_empty(self.get_square(2))
+                        || !piece::is_empty(self.get_square(1))
+                    {
                         continue;
                     }
-                    if self.is_square_controlled(3, piece::WHITE) || self.is_square_controlled(4, piece::WHITE) {
+                    if self.is_square_controlled(3, piece::WHITE)
+                        || self.is_square_controlled(4, piece::WHITE)
+                    {
                         continue;
                     }
                 }
@@ -377,7 +423,11 @@ impl Board {
     pub fn from_index_to_square(index: usize) -> String {
         let file = index % 8;
         let rank = index / 8;
-        format!("{}{}", (file as u8 + b'A') as char, (8 - rank as u8 + b'0') as char)
+        format!(
+            "{}{}",
+            (file as u8 + b'A') as char,
+            (8 - rank as u8 + b'0') as char
+        )
     }
     pub fn from_square_to_index(square: &str) -> usize {
         let file = square.chars().nth(0).unwrap() as usize - b'A' as usize;
